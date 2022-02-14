d31ea3e1033731500e582d3083c5cf3a56f1ef83
Abdullah Foysal <foysal.bd@hotmail.com>
2022-02-14 01:56:11 +0100

    react typescript template complete setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <>
+      <h1>React TypeScript Startup Template. </h1>
+      <p>Environment: {process.env.NODE_ENV}</p>
+      <p>Hostname: {process.env.name}</p>
+    </>
+  )
+}
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const os = require('os')
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify(os.hostname()),
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+}
